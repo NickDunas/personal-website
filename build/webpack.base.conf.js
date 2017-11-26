b219cb5cdb5b5938337fc30e9fed66dd93419687
Nick Dunas <dunas@ecwix.com>
2017-11-26 22:50:19 +0200

    fix: Eslint semi rule

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -1,11 +1,11 @@
 'use strict'
-const path = require('path')
-const utils = require('./utils')
-const config = require('../config')
-const vueLoaderConfig = require('./vue-loader.conf')
+const path = require('path');
+const utils = require('./utils');
+const config = require('../config');
+const vueLoaderConfig = require('./vue-loader.conf');
 
 function resolve (dir) {
-  return path.join(__dirname, '..', dir)
+  return path.join(__dirname, '..', dir);
 }
 
 module.exports = {
@@ -75,4 +75,4 @@ module.exports = {
       }
     ]
   }
-}
+};
